2d78a031889c4ec7d81f09fb1db47f7fb4c087bf
p4triko <168976850+p4triko@users.noreply.github.com>
2024-06-20 13:47:03 +0300

    Temporarily remove decimal button, reorganize button layout

--- script.js
+++ script.js
@@ -1,30 +1,43 @@
-let firstNumber;
-let secondNumber;
+let firstNumber = "";
+let secondNumber = "";
 let operator = "";
+
+let operationArray = new Array(3);
+
+let hasClickedOperator = false;
+let operatorMode = false;
 
 const numberButton = document.querySelectorAll(".number-button");
 const display = document.querySelector(".display");
 const operatorButton = document.querySelectorAll(".operator-button");
 const equalButton = document.querySelector(".equal-button");
 const clearButton = document.querySelector(".clear-button");
-const decimalButton = document.querySelector(".decimal-button");
 
 numberButton.forEach((button) => {
-  button.addEventListener("click", () => {
-    display.textContent += button.textContent;
-  });
+  button.addEventListener("click", appendNumber);
 });
 
-operatorButton.forEach((operator) => {
-  operator.addEventListener("click", () => {
-    display.textContent += operator.textContent;
-  });
+operatorButton.forEach((button) => {
+  button.addEventListener("click", appendOperator);
 });
+
+equalButton.addEventListener(
+  "click",
+  operate(firstNumber, secondNumber, operator)
+);
 
 clearButton.addEventListener("click", clearDisplay);
 
+function appendNumber(event) {}
+
+function appendOperator(event) {}
+
 function clearDisplay() {
-  display.textContent = "";
+  display.textContent = null;
+  firstNumber = null;
+  secondNumber = null;
+  operator = null;
+  hasClickedOperator = false;
 }
 
 function add(a, b) {
