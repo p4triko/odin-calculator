5c3eb5f18cb1d52a0785ae9600da243f82e8b3ce
p4triko <168976850+p4triko@users.noreply.github.com>
2024-06-22 17:12:47 +0300

    New solution, ignore old commented below, change functions and variable names for the new solution

--- script.js
+++ script.js
@@ -1,43 +1,59 @@
-let firstNumber = "";
-let secondNumber = "";
+const numberButton = document.querySelectorAll(".number-button");
+const operatorButton = document.querySelectorAll(".operator-button");
+
+const clearButton = document.querySelector(".clear-button");
+const equalButton = document.querySelector(".equal-button");
+const deleteButton = document.querySelector(".delete-button");
+const decimalButton = document.querySelector(".decimal-button");
+const currentOperation = document.querySelector(".current-operation");
+const previousOperation = document.querySelector(".previous-operation");
+
+let previousValue = "";
+let currentValue = "";
 let operator = "";
 
-let operationArray = new Array(3);
-
-let hasClickedOperator = false;
-let operatorMode = false;
-
-const numberButton = document.querySelectorAll(".number-button");
-const display = document.querySelector(".display");
-const operatorButton = document.querySelectorAll(".operator-button");
-const equalButton = document.querySelector(".equal-button");
-const clearButton = document.querySelector(".clear-button");
-
 numberButton.forEach((button) => {
-  button.addEventListener("click", appendNumber);
+  button.addEventListener("click", (event) => {
+    appendNumber(event.target.textContent);
+    currentOperation.textContent = currentValue;
+  });
 });
 
 operatorButton.forEach((button) => {
-  button.addEventListener("click", appendOperator);
+  button.addEventListener("click", (event) => {
+    appendOperator(event.target.textContent);
+    previousOperation.textContent = previousValue + "" + operator;
+    currentOperation.textContent = currentValue;
+  });
 });
 
-equalButton.addEventListener(
-  "click",
-  operate(firstNumber, secondNumber, operator)
-);
+function appendNumber(number) {
+  if (currentValue.length <= 10) {
+    currentValue += number;
+  }
+}
 
-clearButton.addEventListener("click", clearDisplay);
+function appendOperator(operation) {
+  operator = operation;
+  previousValue = currentValue;
+  currentValue = "";
+}
 
-function appendNumber(event) {}
+function compute() {}
 
-function appendOperator(event) {}
+function updateDisplay() {}
 
-function clearDisplay() {
-  display.textContent = null;
-  firstNumber = null;
-  secondNumber = null;
-  operator = null;
-  hasClickedOperator = false;
+function deleteOperation() {}
+
+clearButton.addEventListener("click", clear);
+
+function clear() {
+  previousValue = "";
+  currentValue = "";
+  operator = "";
+  previousOperation.textContent = currentValue;
+  currentOperation.textContent = currentValue;
+  console.log("Cleared");
 }
 
 function add(a, b) {
@@ -57,18 +73,63 @@ function divide(a, b) {
 }
 
 function operate(first, second, operator) {
-  let result = 0;
   if (operator === "+") {
-    result = add(first, second);
-    return result;
+    return add(first, second);
   } else if (operator === "-") {
-    result = subtract(first, second);
-    return result;
+    return subtract(first, second);
   } else if (operator === "*") {
-    result = multiply(first, second);
-    return result;
+    return multiply(first, second);
   } else if (operator === "/") {
-    result = divide(first, second);
-    return result;
+    return divide(first, second);
+  } else {
+    return null;
   }
 }
+
+// numberButton.forEach((button) => {
+//   button.addEventListener("click", appendNumber);
+// });
+
+// operatorButton.forEach((button) => {
+//   button.addEventListener("click", appendOperator);
+// });
+
+// // deleteButton.addEventListener("click");
+
+// clearButton.addEventListener("click", clearDisplay);
+
+// function appendNumber(number) {
+//   currentDisplay = display.textContent.slice(1);
+//   display.textContent += number.target.textContent;
+//   hasEnteredFirstNumber = true;
+// }
+
+// function appendOperator(opr) {
+//   if (
+//     display.textContent.includes("+") ||
+//     display.textContent.includes("-") ||
+//     display.textContent.includes("*") ||
+//     display.textContent.includes("/")
+//   ) {
+//     return;
+//   } else if (display.textContent === "") {
+//     display.textContent += "";
+//   } else {
+//     firstNumber = display.textContent;
+//     display.textContent = opr.target.textContent;
+//     operator = opr.target.textContent;
+//     hasEnteredOperator = true;
+//     console.log(firstNumber);
+//     console.log(operator);
+//   }
+// }
+
+// function clearDisplay() {
+//   display.textContent = "";
+//   firstNumber = "";
+//   secondNumber = "";
+//   operator = "";
+//   hasEnteredOperator = false;
+//   hasEnteredFirstNumber = false;
+//   console.log("CLEARED");
+// }
